bc268b6b9e6cf4b62e996fa5a80094eca62dde7f
devToseef <dev.toseefahmed@gmail.com>
2024-02-20 14:48:17 +0500

    project files

---
+++ utils/common.js
@@ -0,0 +1,42 @@
+const {redis:redisClient}=require("../lib/redis");
+
+const getTargets=async()=>JSON.parse(await redisClient.get("TARGETS"))||[];
+
+
+const getTargetById=async (targetId)=>{
+    try {
+    const targets=await getTargets();
+    return targets.length?targets.find(({id})=>targetId==id):null;
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
+const getUtcHour=(date)=>new Date(date).getUTCHours();
+
+
+const getUtcDate=(date)=>{
+    console.log("getUtcDate =====> ",date)
+    const utcDate=new Date(date).getUTCDate();
+    const utcMonth=new Date(date).getUTCMonth();
+    const utcYear=new Date(date).getUTCFullYear();
+    return `${utcDate}-${utcMonth}-${utcYear}`
+}
+
+const deleteAll=async()=>{
+    try {
+        redisClient.set("TARGETS",JSON.stringify([]));
+        redisClient.set("SCORE",JSON.stringify({}));
+    } catch (error) {
+        console.log(error);
+        process.exit(0)
+    }
+}
+
+module.exports={
+    getTargets,
+    getTargetById,
+    getUtcHour,
+    getUtcDate,
+    deleteAll
+}
